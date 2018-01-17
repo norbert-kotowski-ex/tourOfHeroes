d3755382ddb81d597cefadb5289960ae14addf3f
nkotowski <norbert.kotowski_external@immobilienscout24.de>
2018-01-17 11:44:29 +0100

    Displaying list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponentComponent } from './hero-detail-component/hero-detail-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponentComponent
   ],
   imports: [
     BrowserModule,
